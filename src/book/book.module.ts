a4e3e1be8a1006ec50e545997f1290469148d197
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-09-12 10:58:13 +0100

    add author entity

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -3,10 +3,12 @@ import { BookService } from './book.service';
 import { BookController } from './book.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookEntity } from './entities/book.entity';
+import { AuthorController } from './author.controller';
+import { AuthorService } from './author.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([BookEntity])],
-  providers: [BookService],
-  controllers: [BookController],
+  providers: [BookService, AuthorService],
+  controllers: [BookController, AuthorController],
 })
 export class BookModule {}
